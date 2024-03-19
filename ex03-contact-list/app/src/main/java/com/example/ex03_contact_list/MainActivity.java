53b755e5e9985d63a8ac5bf933d106be5604fe0e
Bruno Bertholini <brbertholini@gmail.com>
2024-03-18 21:35:13 -0300

    ex3 completo

--- ex03-contact-list/app/src/main/java/com/example/ex03_contact_list/MainActivity.java
+++ ex03-contact-list/app/src/main/java/com/example/ex03_contact_list/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.ex03_contact_list;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputName, inputEmail, inputPhone;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputName = findViewById(R.id.inputName);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPhone = findViewById(R.id.inputPhone);
+        saveButton = findViewById(R.id.saveButton);
+
+    }
+
+    public void gravarContato(View view) {
+        String name = inputName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String phone = inputPhone.getText().toString();
+
+        Log.d("Contato", "Contato salvo. Nome: " + name + ", email: " + email + ", número: " + phone);
     }
 }
